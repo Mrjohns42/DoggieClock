b977dbe82e8e41c55b46bfb55e4a83b323cb2b67
mrjohns42 <mrjohns42@gmail.com>
2020-07-11 19:16:30 -0700

    Fix .gitignore, add example secrets.h

---
+++ example-secrets.h
@@ -0,0 +1,10 @@
+#ifndef _SECRET_H THEN
+#define _SECRET_H
+
+// These are the only two defines you need
+// Save a copy as "secrets.h" and fill out with your information
+
+#define WIFI_SSID "MyWiFiName"
+#define WIFI_PASSWORD "MyWiFiPassw0rd321!"
+
+#endif
